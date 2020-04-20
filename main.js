750edffc4cc9463477e6eef9ae649c051df5dfec
evan.m.reynolds95@gmail.com <evanr95@MSI.localdomain>
2020-04-20 12:27:35 -0500

    inital commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let bandNumber = 1
+
+const takeNumber = function(bandName) {
+  return `${bandNumber++}. ${bandName}`;
+};
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
+
+const knockedLoose = takeNumber("Knocked Loose")
+console.log(knockedLoose)
